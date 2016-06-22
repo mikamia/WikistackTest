f180a34befe9f09ac801eb38a0f965cee458c582
Danielle Gellis <danisyellis@gmail.com>
2016-06-22 15:10:43 -0400

    add first 2 tests and a gitignore file

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,1 +1,15 @@
-console.log("HAHAHAHAH");
+var expect = require("chai").expect;
+
+describe("Testing tests", function() {
+	it("checks that 2+2 = 4", function() {
+		expect(2 + 2).to.equal(4);
+	});
+	it("setTimeout waits for the right amount of time", function(done) {
+		var start = new Date();
+		setTimeout(function() {
+			var duration = new Date() - start;
+			expect(duration).to.be.closeTo(1000, 50);
+			done();
+		}, 1000);
+	});
+});
