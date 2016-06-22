bc69f2f58a207abdb9dea248064a8444a71a91e4
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-22 15:26:51 -0400

    tests passed

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,9 +1,15 @@
 var expect = require("chai").expect;
+var chai = require('chai');
+var spies = require('chai-spies');
+chai.use(spies);
+
 
 describe("Testing tests", function() {
+
 	it("checks that 2+2 = 4", function() {
 		expect(2 + 2).to.equal(4);
 	});
+
 	it("setTimeout waits for the right amount of time", function(done) {
 		var start = new Date();
 		setTimeout(function() {
@@ -12,4 +18,15 @@ describe("Testing tests", function() {
 			done();
 		}, 1000);
 	});
+
+  it('will invoke a function once per element', function () {
+    var arr = ['x','y','z'];
+      function logNth (val, idx) {
+        console.log('Logging elem #'+idx+':', val);
+      }
+    var logNth = chai.spy(logNth);
+    arr.forEach(logNth);
+    expect(logNth).to.have.been.called.exactly(arr.length);
+  });
+
 });
