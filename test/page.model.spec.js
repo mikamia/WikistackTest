270403c8c56141d9a521a6155bf29768ce7dde39
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-22 17:05:39 -0400

    class methods

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -11,6 +11,7 @@ describe('Pages model', function () {
 		page = Page.build();
 	});
 
+
 	describe('route', function () {
 		it('returns the url_name prepended by "/wiki/"', function() {
 			page.urlTitle = 'some_title';
@@ -23,5 +24,49 @@ describe('Pages model', function () {
       		page.content = "some content";
       		expect(page.renderedContent).to.equal('<p>'+page.content+'</p>\n');
       });
-    });
+  });
+
+	describe('Class methods', function () {
+
+		beforeEach(function (done) {
+  		Page.create({
+		    title: 'foo',
+		    content: 'bar',
+		    tags: ['foo', 'bar']
+  		})
+  		.then(function () {
+    		done();
+  		})
+  		.catch(done);
+		});
+
+		afterEach(function (done){
+			Page.sync({force:true}).then(function(){
+				done();
+			}).catch(done);
+
+		});
+
+
+	  describe('findByTag', function () {
+      it('gets pages with the search tag', function(done){
+      	Page.findByTag('foo').then(function(page){
+      		//console.log(page);
+      		//expect(tomato).to.exist;
+      		//expect(undefined).to.exist;
+      		expect(page).to.exist;
+      		done();
+      	}).catch(done);
+
+      });
+      it('does not get pages without the search tag', function(done){
+      	Page.findByTag(['foo','bar']).then(function(pages){
+      		expect(pages.length).to.equal(1);
+      		done();
+      	}).catch(done);
+      });
+	  });
+	});
+
+
 });
